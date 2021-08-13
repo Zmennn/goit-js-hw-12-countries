b9e35e3d518857856ef673f5d093feed3590c051
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-08-13 22:38:26 +0300

    0.3

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export default refs;
+const refs = {
+    input: document.querySelector('.input-section__input'),
+    caution: document.querySelector('.caution-container'),
+    output: document.querySelector('.output-section'),
+}
